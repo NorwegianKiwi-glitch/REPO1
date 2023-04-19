1a4a905c261016750548aa8331117821b47243b8
NorwegianKiwi-glitch <simonhelgen1@gmail.com>
2023-04-19 10:36:26 +0000

    bravo

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/NorwegianKiwi-glitch/REPO1
+
+go 1.18
+
+// required github.com/NorwegianKiwi-glitch/REPO3/mycrypt
+
+require github.com/NorwegianKiwi-glitch/REPO3 v0.0.0-20230418134510-e4107c72e56f
--- tcp_server.go
+++ tcp_server.go
@@ -10,26 +10,34 @@ import (
 )
 
 func main() {
-
     var wg sync.WaitGroup
 
-    server, err := net.Listen("tcp", "172.17.0.3:90901")
+    server, err := net.Listen("tcp", "172.17.0.2:")
     if err != nil {
         log.Fatal(err)
     }
+    defer server.Close()
 
     log.Printf("bundet til %s", server.Addr().String())
+
     wg.Add(1)
     go func() {
         defer wg.Done()
+
         for {
             log.Println("før server.Accept() kallet")
+
             conn, err := server.Accept()
             if err != nil {
-                return
+                log.Println(err)
+                continue
             }
+
+            wg.Add(1)
             go func(c net.Conn) {
-                defer c.Close()
+                defer wg.Done()
+                defer conn.Close()
+
                 for {
                     buf := make([]byte, 1024)
                     n, err := c.Read(buf)
@@ -37,27 +45,32 @@ func main() {
                         if err != io.EOF {
                             log.Println(err)
                         }
-                        return // fra for løkke
+                        return
                     }
+
                     dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
                     log.Println("Dekrypter melding: ", string(dekryptertMelding))
+
                     switch msg := string(dekryptertMelding); msg {
                     case "ping":
-                        kryptertMelding := mycrypt.Krypter([]rune("pong"), mycrypt.ALFSEM03, 4)
+                        kryptertMelding := mycrypt.Krypter([]rune("pong"), mycrypt.ALF_SEM03, 4)
                         log.Println("Kryptert melding: ", string(kryptertMelding))
-                        , err = c.Write([]byte(string(kryptertMelding)))
+                        _, err = c.Write([]byte(string(kryptertMelding)))
+
                     default:
                         _, err = c.Write(buf[:n])
                     }
+
                     if err != nil {
                         if err != io.EOF {
                             log.Println(err)
                         }
-                        return // fra for løkke
+                        return
                     }
                 }
             }(conn)
         }
     }()
+
     wg.Wait()
 }
